ea85af650a6b34122019b4c08d22187f57aee562
shivmSingh1 <shivams7380@gmail.com>
2025-09-01 11:32:50 +0530

    added react persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
-import Store from './redux/store.js';
+import Store, { persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={Store} >
+       <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+ 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const Store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
       serializableCheck: false
@@ -12,4 +20,5 @@ const Store = configureStore({
   devTools: process.env.NODE_ENV === "development"
 })
 
+export const  persistor = persistStore(Store)
 export default Store;
