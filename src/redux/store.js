af3bb4dbe1d914d9108f1a0e61744b67c515ba1f
shivmSingh1 <shivams7380@gmail.com>
2025-09-01 11:50:43 +0530

    react persist encryption

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { encryptTransform } from 'redux-persist-transform-encrypt';
  
 const persistConfig = {
   key: 'root',
   storage,
+  transforms: [
+      encryptTransform({
+        secretKey:import.meta.env.VITE_PERSIST_SECRET_KEY,
+        onError: function (error) {
+          console.log("something went wrong while encrypting persist store")
+        },
+      }),
+    ],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
